723e34f20c774c608fee1331d72a186d690cfdc8
AmitayManor <amitay911@gmail.com>
2025-01-20 22:42:26 +0200

    initialized models an users table CRUD ops

---
+++ api/handlers/users.go
@@ -0,0 +1,289 @@
+package handlers
+
+import (
+	"api/models"
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/supabase-community/supabase-go"
+)
+
+type UserHandler struct {
+	supabaseClient *supabase.Client
+}
+
+func NewUserHandler(supabaseClient *supabase.Client) *UserHandler {
+	return &UserHandler{
+		supabaseClient: supabaseClient,
+	}
+}
+
+func (h *UserHandler) CreateUser(c *fiber.Ctx) error {
+	var user models.User
+
+	// Verify body is correct
+	if err := c.BodyParser(&user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid body request",
+		})
+	}
+
+	// Check there is no users as the new one
+	_, count, err := h.supabaseClient.From("users").
+		Select("id, email", "exact", false).
+		Eq("email", user.Email).
+		Execute()
+
+	if err == nil && count > 0 {
+		return c.Status(fiber.StatusConflict).JSON(fiber.Map{
+			"error": models.ErrUserAlreadyExists.Error(),
+		})
+	}
+
+	// Create the new user
+	_, _, err = h.supabaseClient.From("users").
+		Insert(user, false, "", "representation", "exact").
+		Execute()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to create user",
+		})
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(models.UserResponse{
+		ID:       user.ID,
+		Email:    user.Email,
+		IsAdmin:  user.IsAdmin,
+		IsActive: user.IsActive,
+	})
+}
+
+func (h *UserHandler) GetUser(c *fiber.Ctx) error {
+	userID := c.Params("id")
+	id, err := uuid.Parse(userID)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": models.ErrUserNotFound.Error(),
+		})
+	}
+
+	result, count, err := h.supabaseClient.From("users").
+		Select("*", "exact", false).
+		Eq("id", id.String()).
+		Execute()
+
+	if err != nil || count == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": models.ErrUserNotFound.Error(),
+		})
+	}
+
+	var user models.User
+	if err := json.Unmarshal(result, &user); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to parse response",
+		})
+	}
+
+	return c.JSON(models.UserResponse{
+		ID:       user.ID,
+		Email:    user.Email,
+		IsAdmin:  user.IsAdmin,
+		IsActive: user.IsActive,
+	})
+}
+
+func (h *UserHandler) ListUsers(c *fiber.Ctx) error {
+	// pagination
+	page := c.QueryInt("page", 1)
+	limit := c.QueryInt("limit", 10)
+	offset := (page - 1) * limit
+
+	res, count, err := h.supabaseClient.From("users").
+		Select("*", "exact", false).
+		Range(offset, offset+limit-1, "").
+		Execute()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to fetch users",
+		})
+	}
+
+	var users []models.User
+	if err := json.Unmarshal(res, &users); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to parse response",
+		})
+	}
+
+	response := make([]models.UserResponse, len(users))
+	for i, user := range users {
+		response[i] = models.UserResponse{
+			ID:       user.ID,
+			Email:    user.Email,
+			IsAdmin:  user.IsAdmin,
+			IsActive: user.IsActive,
+		}
+	}
+
+	return c.JSON(fiber.Map{
+		"users": response,
+		"total": count,
+		"page":  page,
+		"limit": limit,
+	})
+}
+
+func (h *UserHandler) UpdateUser(c *fiber.Ctx) error {
+	userId := c.Params("id")
+	id, err := uuid.Parse(userId)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": models.ErrInvalidCredentials.Error(),
+		})
+	}
+
+	var updateData models.User
+	if err := c.BodyParser(&updateData); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Invalid Body request",
+		})
+	}
+
+	_, count, err := h.supabaseClient.From("users").
+		Select("*", "exact", false).
+		Eq("id", id.String()).
+		Execute()
+
+	if err != nil || count == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": models.ErrUserNotFound.Error(),
+		})
+	}
+
+	_, _, err = h.supabaseClient.From("users").
+		Update(updateData, "representaion", "exact").
+		Eq("id", id.String()).
+		Execute()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to update user",
+		})
+	}
+
+	return c.SendStatus(fiber.StatusOK)
+}
+
+func (h *UserHandler) DeleteUser(c *fiber.Ctx) error {
+	userId := c.Params("id")
+	id, err := uuid.Parse(userId)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": models.ErrInvalidCredentials.Error(),
+		})
+	}
+
+	// using Soft delete by turning Is_Active to False instead of Hard deleting
+	updateData := map[string]interface{}{"is_active": false}
+	_, _, err = h.supabaseClient.From("users").
+		Update(updateData, "representation", "exact").
+		Eq("id", id.String()).
+		Execute()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to delete user",
+		})
+	}
+
+	return c.SendStatus(fiber.StatusOK)
+}
+
+func (h *UserHandler) UpdateLoginAttempts(c *fiber.Ctx) error {
+	userId := c.Params("id")
+	id, err := uuid.Parse(userId)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": models.ErrInvalidCredentials.Error(),
+		})
+	}
+
+	res, count, err := h.supabaseClient.From("users").
+		Select("failed_login_attempts", "exact", false).
+		Eq("id", id.String()).
+		Execute()
+
+	if err != nil || count == 0 {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": models.ErrUserNotFound.Error(),
+		})
+	}
+
+	var user models.User
+	if err := json.Unmarshal(res, &user); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to parse response",
+		})
+	}
+
+	updateData := map[string]interface{}{
+		"failed_login_attempts": user.FailedLoginAttempts + 1,
+		"is_active":             user.FailedLoginAttempts < 4,
+	}
+
+	_, _, err = h.supabaseClient.From("users").
+		Update(updateData, "representation", "exact").
+		Eq("id", id.String()).
+		Execute()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to update login attempts",
+		})
+	}
+
+	return c.SendStatus(fiber.StatusOK)
+}
+
+func (h *UserHandler) ResetLoginAttempts(c *fiber.Ctx) error {
+	userId := c.Params("id")
+	id, err := uuid.Parse(userId)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": models.ErrInvalidCredentials.Error(),
+		})
+	}
+
+	updateDate := map[string]interface{}{
+		"failed_login_attempts": 0,
+		"last_login":            "NOW()",
+	}
+
+	_, _, err = h.supabaseClient.From("users").
+		Update(updateDate, "representation", "exact").
+		Eq("id", id.String()).
+		Execute()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to reset login attempts",
+		})
+	}
+
+	return c.SendStatus(fiber.StatusOK)
+}
+
+func (h *UserHandler) AdminUpdateUser(c *fiber.Ctx) error {
+	reqUserData := c.Locals("user")
+	reqUser, ok := reqUserData.(models.User)
+	if !ok || !reqUser.IsAdmin {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": models.ErrUnauthorized.Error(),
+		})
+	}
+
+	return h.UpdateUser(c)
+}
