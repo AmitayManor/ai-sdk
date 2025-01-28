eda33d3c8b6ed77ef4ec930f00902a86f078062c
AmitayManor <amitay911@gmail.com>
2025-01-28 18:27:04 +0200

    changed the edge function

--- supabase/functions/huggingface-models/index.ts
+++ supabase/functions/huggingface-models/index.ts
@@ -1,3 +1,7 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
 import { serve } from "https://deno.land/std@0.214.0/http/server.ts"
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2.48.1"
 import { HuggingFaceClient } from './../../shared/huggingface.ts'
@@ -72,3 +76,15 @@ serve(async (req: Request) => {
     return handleError(error)
   }
 })
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/huggingface-models' \
+    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
